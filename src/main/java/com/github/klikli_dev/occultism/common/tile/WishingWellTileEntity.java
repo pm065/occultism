9e44bd8db67e6a216b664c3d2b4923ab0ec5a4d4
klikli-dev <klikli@outlook.at>
2020-04-04 13:20:20 +0200

    added fluid spawning and slow tick verification of multiblock

--- src/main/java/com/github/klikli_dev/occultism/common/tile/WishingWellTileEntity.java
+++ src/main/java/com/github/klikli_dev/occultism/common/tile/WishingWellTileEntity.java
@@ -24,18 +24,74 @@ package com.github.klikli_dev.occultism.common.tile;
 
 import com.github.klikli_dev.occultism.registry.OccultismBlocks;
 import com.github.klikli_dev.occultism.registry.OccultismTiles;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.tileentity.ITickableTileEntity;
+import net.minecraft.util.math.BlockPos;
 
-public class WishingWellTileEntity extends NetworkedTileEntity {
+import java.util.concurrent.ThreadLocalRandom;
+
+public class WishingWellTileEntity extends NetworkedTileEntity implements ITickableTileEntity {
+    //region Fields
+    /**
+     * Init flag for water spawning
+     */
+    protected boolean hasEverSpawnedWater;
+    /**
+     * Init flag for multiblock check.
+     */
+    protected boolean hasEverValidatedMultiblock;
+    /**
+     * Can be outdated, is updated every 40-60 seconds.
+     */
+    public boolean hasValidMultiblock;
+
+    /**
+     * the ticks between multiblock check ticks.
+     */
+    protected int multiBlockTickTime;
+    //endregion Fields
+
     //region Initialization
     public WishingWellTileEntity() {
         super(OccultismTiles.WISHING_WELL.get());
+        //randomize the tick time between 40-60 seconds to avoid all TEs checking in sync.
+        this.multiBlockTickTime = ThreadLocalRandom.current().nextInt(40, 60) * 20;
     }
     //endregion Initialization
 
+    //region Overrides
+    @Override
+    public void tick() {
+        if (!this.hasEverValidatedMultiblock) {
+            this.hasEverValidatedMultiblock = true;
+            this.hasValidMultiblock = this.hasValidMultiblock();
+        }
+        if (!this.hasEverSpawnedWater && this.hasValidMultiblock) {
+            this.hasEverSpawnedWater = true;
+            this.spawnFluidColumn();
+        }
+
+        //check multiblock and water every 40-60 seconds.
+        if (this.world.getGameTime() % this.multiBlockTickTime == 0) {
+            this.hasValidMultiblock = this.hasValidMultiblock();
+            if (this.hasValidMultiblock) {
+                this.spawnFluidColumn();
+            }
+        }
+    }
+
+    //endregion Overrides
 
     //region Methods
     public boolean hasValidMultiblock() {
         return OccultismBlocks.WISTHING_WELL.get().blockMatcher.validate(this.world, this.pos) != null;
+    }
+
+    protected void spawnFluidColumn() {
+        BlockPos fluidPos = this.pos.add(0, 3, 0);
+        if (this.world.getBlockState(fluidPos).getFluidState().getFluid() != Fluids.WATER) {
+            this.world.setBlockState(fluidPos, Fluids.WATER.getDefaultState().getBlockState(), 11);
+        }
     }
     //endregion Methods
 }
