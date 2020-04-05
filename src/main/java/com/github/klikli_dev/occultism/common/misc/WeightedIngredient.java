260c07a07bee70069cddf72d86b61e47a7054bb6
klikli-dev <klikli@outlook.at>
2020-04-05 12:54:14 +0200

    fixed potential null itemstack in weighted ingredient

--- src/main/java/com/github/klikli_dev/occultism/common/misc/WeightedIngredient.java
+++ src/main/java/com/github/klikli_dev/occultism/common/misc/WeightedIngredient.java
@@ -34,7 +34,7 @@ public class WeightedIngredient extends WeightedRandom.Item {
     }
 
     public ItemStack getStack(){
-        return this.ingredient.getMatchingStacks()[0];
+        return this.ingredient.hasNoMatchingItems() ? ItemStack.EMPTY : this.ingredient.getMatchingStacks()[0];
     }
 
     public Ingredient getIngredient() {
